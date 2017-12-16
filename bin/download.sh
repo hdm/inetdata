d0b764f26c4f70f9e22faf904105697b49f0ec0f
HD Moore <x@hdm.io>
2017-12-16 09:03:45 -0600

    Add bashrc to env

--- bin/download.sh
+++ bin/download.sh
@@ -4,5 +4,9 @@ if [ -f /etc/profile.d/rvm.sh  ]; then
   source /etc/profile.d/rvm.sh
 fi
 
+if [ -f ${HOME}/.bashrc ]; then
+  source ${HOME}/.bashrc
+fi
+
 THIS=$(readlink -f "$(dirname "$(readlink -f "$0")")/")
 exec ${THIS}/download.rb $@
--- bin/normalize.sh
+++ bin/normalize.sh
@@ -4,7 +4,12 @@ if [ -f /etc/profile.d/rvm.sh  ]; then
   source /etc/profile.d/rvm.sh
 fi
 
+if [ -f ${HOME}/.bashrc ]; then
+  source ${HOME}/.bashrc
+fi
+
 export PATH=/usr/local/bin:$PATH
 export GODEBUG=cgocheck=0
+
 THIS=$(readlink -f "$(dirname "$(readlink -f "$0")")/")
 exec ${THIS}/normalize.rb $@
